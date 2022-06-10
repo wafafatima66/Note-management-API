70e1d3b2a011dec441e4cfcb6f52b97823af37b3
Mohammed Nayeem <chiefnayeem@gmail.com>
2022-06-11 03:30:10 +0600

    Fixed room list sql query - api

--- app/Helpers/SQLQueryHelper.php
+++ app/Helpers/SQLQueryHelper.php
@@ -30,6 +30,9 @@ class SQLQueryHelper
                                 (SELECT COUNT(*)
                                  FROM message_connection_archived MCA
                                  WHERE MCA.connection_id = MC.id AND MCA.user_id = " . $user_id . ") AS archived,
+                                 (SELECT CASE
+                                    WHEN MC.room_type = 'one-to-one' THEN (SELECT display_name FROM users U WHERE U.id = (SELECT MCU.user_id FROM message_connection_users MCU WHERE MCU.connection_id = MC.id AND MCU.user_id <> 1 LIMIT 1))
+                                    END)                                              AS opponent_title,
                                 MC.created_at                                         AS created_at,
                                 MC.updated_at                                         AS updated_at
                          FROM message_connections MC
