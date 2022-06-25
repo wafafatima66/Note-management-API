97169cf57b7ab5169cf48a066265bd99bd0b9366
Wafa Fatima <wafafatima66@gmail.com>
2022-06-25 19:58:35 +0600

    created user application model and migration

---
+++ database/migrations/2022_06_25_135213_user_installed_applications.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::create('user_installed_applications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('application_id')->constrained('applications')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
